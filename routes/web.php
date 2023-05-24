fee391e10ef10c9aa8b71437c931ef1ff7dc2c53
dat <21211tt0848@mail.tdc.edu.vn>
2023-05-24 10:36:45 +0700

    curd_category

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\UserController;
+use App\Models\Product;
+use App\Models\ProductDetail;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Intervention\Image\Size;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +16,66 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard.index');
+})->name('dashboard');
+Route::get('/home', function () {
+    return view('client.layouts.app');
+});
+Auth::routes();
+
+Route::resource('roles', RoleController::class);
+Route::resource('users', UserController::class);
+Route::resource('categories', CategoryController::class);
+Route::resource('products', ProductController::class);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::post('/create ', function(Request $request){
+
+    $dataCreate = $request->except('sizes');
+    $sizes = $request->sizes ? json_decode($request->sizes) : [];
+
+    $product = Product::create($dataCreate);
+    $sizeArray = [];
+    foreach($sizes as $size)
+    {
+        $sizeArray[] = ['size' => $size->size, 'quantity' => $size->quantity, 'product_id' => $product->id];
+    }
+    ProductDetail::insert($sizeArray);
+})->name('test');
